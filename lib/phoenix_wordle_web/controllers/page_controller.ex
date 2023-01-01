16fb2ef2b73369c63087f1e5735bba4dda64adce
extazystas <2153311+Extazystas@users.noreply.github.com>
2023-01-01 23:49:58 +0200

    Assign vars in controller

--- lib/phoenix_wordle_web/controllers/page_controller.ex
+++ lib/phoenix_wordle_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule PhoenixWordleWeb.PageController do
   use PhoenixWordleWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", page_text: 'helloo live view')
   end
 end
